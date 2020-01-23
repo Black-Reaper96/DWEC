cb3a051558815467bdfbc252dd1456711ddec950
Alex <am1996.am98@gmail.com>
2020-01-23 18:11:56 +0100

    terminado vue route

--- VueCLI/bootstrap/src/App.vue
+++ VueCLI/bootstrap/src/App.vue
@@ -2,9 +2,7 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/servicios">Servicios</router-link> | 
-      <router-link to="/fotos/1">Fotos</router-link>
+      <router-link to="/about">About</router-link>
     </div>
     <router-view/>
   </div>
--- VueCLI/bootstrap/src/router/index.js
+++ VueCLI/bootstrap/src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
@@ -7,22 +8,15 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: () => import(/* webpackChunkName: "about" */ '../views/Home.vue')
+    component: Home
   },
   {
     path: '/about',
     name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
-  {
-    path: '/servicios',
-    name: 'servicios',
-    component: () => import(/* webpackChunkName: "servicios" */ '../views/Servicios.vue')
-  },
-  {
-    path: '/fotos/:id',
-    name: 'fotos',
-    component: () => import(/* webpackChunkName: "fotos" */ '../views/Fotos.vue')
   }
 ]
 

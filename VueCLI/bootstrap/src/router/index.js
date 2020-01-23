830f3bd9fbfaf6cabedcc76206d8c9a046cb8d20
Alex <am1996.am98@gmail.com>
2020-01-23 21:35:52 +0100

    Continuamos con vue+bootstrap

--- VueCLI/bootstrap/src/router/index.js
+++ VueCLI/bootstrap/src/router/index.js
@@ -17,7 +17,23 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },
+  {
+    path: '/grid',
+    name: 'grid',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Grid.vue')
+  },
+  {
+    path: '/cards',
+    name: 'cards',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Cards.vue')
+  },
 ]
 
 const router = new VueRouter({

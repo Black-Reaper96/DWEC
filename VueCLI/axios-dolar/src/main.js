5b154086355c6b699010d064ee41c13d789c7db2
Alex <am1996.am98@gmail.com>
2020-01-29 21:19:25 +0100

    Terminamos Bootstrap empezamos vuetify

--- VueCLI/axios-dolar/src/main.js
+++ VueCLI/axios-dolar/src/main.js
@@ -1,19 +1,16 @@
 import Vue from 'vue'
-import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
-Vue.use(BootstrapVue);
-Vue.use(BootstrapVueIcons);
-
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+import vuetify from './plugins/vuetify';
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
---
+++ VueCLI/bootstrap/src/views/Formulario.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="mt-3">
+        <h1>Formularios</h1>
+
+        <form>
+            <b-row>
+                <b-col md="3">
+                    <b-form-input type="text" v-model="texto" :state="comprobar"></b-form-input>
+                    <small>Minimo 5 caracteres</small>
+                </b-col>
+                <b-col md="3">
+                    <b-form-select v-model="select" :options="animales"></b-form-select>
+                    <p>Seleccion : {{select}}</p>
+                </b-col>
+                <b-col md="3">
+                    <b-form-radio-group v-model="radio" :options="animalesRad"></b-form-radio-group>
+                    <p>Radio: {{radio}}</p>
+                </b-col>
+                <b-col md="3">
+                    <b-form-checkbox-group v-model="check" :options="animalesRad"></b-form-checkbox-group>
+                    <p>Multiple {{check}}</p>
+                </b-col>
+            </b-row>
+        </form>
+        <p>{{texto}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'Form',
+    data(){
+        return{
+            texto: '',
+            select: null,
+            radio: null,
+            check: [],
+            animales:[
+                {value: null, text: 'Seleccione un animal'},
+                {value: 'perro', text: 'Guau guau'},
+                {value: 'gato', text: 'Miau'},
+                {value: 'pato', text: 'Cuack'},
+            ],
+            animalesRad:[
+                {value: 'perro', text: 'Guau guau'},
+                {value: 'gato', text: 'Miau'},
+                {value: 'pato', text: 'Cuack'},
+            ],
+        }
+    },
+    computed:{
+        comprobar(){
+            return this.texto.length > 4 ? true : false
+        }
+    }
+}
+</script>
